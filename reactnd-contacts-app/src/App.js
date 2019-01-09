b3149f5c93c3efba0b2d75ecbf3c3fe4810584b7
Camila <salescamila1@gmail.com>
2019-01-08 20:47:19 -0400

    StateBased Content Rendering

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: 'create'
   }
 
   componentDidMount() {
@@ -33,10 +35,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === 'list' && 
+          (<ListContacts 
           contacts={this.state.contacts} 
           onDeleteContact={this.removeContact}
-        />
+          />)
+        }
+        {this.state.screen === 'create' && 
+          (<CreateContact />)
+        }
       </div>
     )
   }
